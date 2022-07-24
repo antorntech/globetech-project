22103780cf8e2e1a6d33a842f0a5bbe09112fdff
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-24 15:48:39 +0600

    install react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div></div>;
 }
 
 export default App;
