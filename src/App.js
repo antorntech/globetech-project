3fe74a97df18662ad76fd28edd5179766a02b851
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-24 16:04:06 +0600

    create navbar in header folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Services from "./Pages/Services/Services";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,37 +2,30 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            Globe<span style={{ color: "orange" }}>Tech</span>{" "}
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/services">
+                Services
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Login
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
