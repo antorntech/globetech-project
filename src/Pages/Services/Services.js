3f037191f9b8688a53cb651d431b783fb01a8ea7
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-24 18:12:14 +0600

    add service header

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,9 +1,37 @@
 import React from "react";
+import line1 from "../../images/Line 2.png";
+import line2 from "../../images/Line 3.png";
+import line3 from "../../images/Line 4.png";
+import plane from "../../images/fa_space-shuttle.png";
+import "./Services.css";
 
 const Services = () => {
   return (
-    <div>
-      <h1>Hi! This is services page.</h1>
+    <div className="container">
+      <div className="row row-cols-1 py-3">
+        <div className="col">
+          <div className="d-flex align-items-center moves">
+            <div className="hero-line">
+              <div id="line1">
+                <img src={line1} alt="" />
+              </div>
+              <div id="line2">
+                <img src={line2} alt="" />
+              </div>
+              <div id="line3">
+                <img src={line3} alt="" />
+              </div>
+            </div>
+            <div>
+              <img src={plane} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <header className="text-center text-white mt-3">
+        <h1>Services</h1>
+        <p className="text-white">We provides services to our clients</p>
+      </header>
     </div>
   );
 };
